ebe23794d0ab4a3e0692a703c4704cc13a9dfa45
4141 <jasiekfanarty@gmail.com>
2021-05-18 20:38:08 +0200

    todos

--- main.cpp
+++ main.cpp
@@ -13,29 +13,30 @@ int main() {
             tree = BTree(order);
         } else if (choice == 'A') {
             cin >> val;
-            tree.insert(val);
+            //tree.insert(val); TODO:
         } else if (choice == '?') {
             cin >> val;
             cout << val;
-            //tree.search(val) ? cout << " +\n" : cout << " -\n";
+            //tree.search(val) ? cout << " +\n" : cout << " -\n"; TODO:
         } else if (choice == 'P') {
-            //tree.traverse();
+            //tree.traverse(); TODO:
             cout << endl;
         } else if (choice == 'L') {
             cin >> order;
             tree = BTree(order);
         } else if (choice == 'S') {
-            // TODO
+            // TODO:
         } else if (choice == 'R') {
             cin >> val;
-            //tree.remove(val);
+            //tree.remove(val);TODO:
         } else if (choice == '#') {
             ;;
         } else if (choice == 'X') {
             //while (tree.root != nullptr) tree.remove(tree.root->keys[tree.root->currentKeys - 1]);
+            // TODO:
             return 1;
         } else if (choice == 'C') {
-            // TODO
+            // TODO:
         } else {
             cout << "Nie znam komendy : " << choice << endl;
         }
